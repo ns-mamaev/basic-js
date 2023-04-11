3d20bcc7847646961b7bbf1ee06e5d36606e96ab
ns-mamaev <palmally@yandex.ru>
2023-04-12 00:54:44 +0300

    feat: complete 7 tasks (Wed Apr 12 2023 00:54:21 GMT+03)

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  for (let c of s1) {
+    s2 = s2.replace(c, '^_^')
+  }
+  return s2.match(/\^_\^/g)?.length || 0;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const dict = [];
+  for (const c of str) {
+    if (dict[dict.length - 1]?.[0] === c) {
+      dict[dict.length - 1].push(c);
+    } else {
+      dict.push([c]);
+    }
+  }
+  return dict.reduce((acc, curr) => acc + (curr.length > 1 ? curr.length + curr[0] : curr[0]),'')
 }
 
 module.exports = {
