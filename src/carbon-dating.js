39d84c523b7d3fdfe051735e16e795974e52a0d6
ns-mamaev <palmally@yandex.ru>
2023-04-15 14:47:46 +0300

    feat: complete cipher task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || !Number(sampleActivity)) {
+    return false;
+  }
+
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  const A0 = MODERN_ACTIVITY;
+  const A = Number(sampleActivity);
+
+  if (A <= 0 || A > A0) {
+    return false;
+  }
+
+  const res = Math.ceil(Math.log( A0 / A) / k)
+  return res;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  checkArguments(...args) {
+    const checkRes = args.length === 2 && args.every(arg => typeof arg === 'string');
+    if (!checkRes) {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+
+  getCodes(str) {
+    return str.toUpperCase()
+    .split('')
+    .map(char => {
+      const charCode = char.charCodeAt(0);
+      return charCode >= 65 && charCode <= 90 ? charCode : char; 
+    });
+  }
+
+  getMessageWithKey(message, key, type = 'encrypt') {
+    this.checkArguments(message, key)
+
+    const textCodes = this.getCodes(message);
+    const keyCodes = this.getCodes(key);
+
+    const res = [];
+
+    let keyIndex = 0;
+    for (const textCode of textCodes) {
+      if (typeof textCode === 'number') {
+        const keyCode = keyCodes[keyIndex];
+        keyIndex = (keyIndex + 1) % (keyCodes.length);
+        
+        let secretCode;
+
+        if (type === 'decrypt') {
+          const codesDelta = textCode - keyCode;
+          secretCode = codesDelta < 0 ? codesDelta + 91 : codesDelta + 65;
+        } else {
+          secretCode = (keyCode + textCode - 130) % 26 + 65;
+        }
+
+        const secretChar = String.fromCharCode(secretCode);
+        res.push(secretChar);
+      } else {
+        res.push(textCode)
+      }
+    }
+
+    return res;
+  }
+
+  getStringFromArr(arr) {
+    return this.isDirect ? arr.join('') : arr.reverse().join('');
+  }
+
+  encrypt(message, key) {
+    const encrypted = this.getMessageWithKey(message, key, 'encrypt');
+    return this.getStringFromArr(encrypted);
+  };
+
+
+  decrypt(encriptedMessage, key) {
+    const decrypted = this.getMessageWithKey(encriptedMessage, key, 'decrypt');
+    return this.getStringFromArr(decrypted);
   }
 }
 
